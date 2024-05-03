1f3546b56e0df8e60a07bbf5e5ec537d2ab66b10
LazY <darshitmistry2@gmail.com>
2024-05-03 20:42:05 +0530

    style : Home page Header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/header-box'
+import TotalBalanceBox from '@/components/total-balance-box'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { 
+    firstName : "Darshit"
+  }
   return (
-    <div>Home</div>
+    // class will imported by the globals.css
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext = "Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks = {1}
+          totalCurrentBalance = {1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
