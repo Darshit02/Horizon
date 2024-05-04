9efaa7762f3647f6e9982f7b3c72e964a08a1cc6
LazY <darshitmistry2@gmail.com>
2024-05-04 17:35:27 +0530

    feat : heropage right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from '@/components/header-box'
+import RightSidebar from '@/components/right-sidebar'
 import TotalBalanceBox from '@/components/total-balance-box'
 import React from 'react'
 
 const Home = () => {
   const loggedIn = { 
-    firstName : "Darshit"
+    firstName : "Darshit",
+    lastName : "Mistry",
+    email : "darshitmistry2@gmail.com"
   }
   return (
     // class will imported by the globals.css
@@ -23,7 +26,13 @@ const Home = () => {
           totalCurrentBalance = {1250.35}
           />
         </header>
+        RECENT TRANSACTION
       </div>
+      <RightSidebar
+      user = {loggedIn}
+      transactions = {[]}
+      banks = {[{currentBalance : 1234.22},{currentBalance : 11222.22}]}
+      />
     </section>
   )
 }
