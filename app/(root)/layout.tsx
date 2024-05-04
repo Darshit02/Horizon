177f4f6f07cdb3c82ca5282d424f9cba8cb770fd
LazY <darshitmistry2@gmail.com>
2024-05-04 09:05:30 +0530

    feat : sidebar for mobile and large screen

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,29 @@
+import MobileNav from "@/components/mobile-navbar";
+import Sidebar from "@/components/sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName : "Darshit",
+    lastName : "Mistry"
+  }
   return (
-   <main>
-    SIDEBAR
-    {children}
-   </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
