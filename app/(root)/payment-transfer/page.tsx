644f2fc912b24efa7046426adbf7401499c5cf31
LazY <darshitmistry2@gmail.com>
2024-05-09 10:20:02 +0530

    feat : fund transfer state and checkout

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from '@/components/header-box'
+import PaymentTransferForm from '@/components/payment-transfer-form'
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const PaymentTransfer = () => {
+const PaymentTransfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
+  })
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data || [];
   return (
-    <div>PaymentTransfer</div>
+    <section className='payment-transfer'>
+      <HeaderBox
+        title='Payment Transfer'
+        subtext='Transfer money to your friends and family in a few simple steps.'
+      />
+      <section className='size-full pt-5'>
+        <PaymentTransferForm
+          accounts={accountsData}
+        />
+      </section>
+    </section>
   )
 }
 
